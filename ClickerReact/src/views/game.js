638f40f630f750a4631568c8f2cac4738dbeb3a4
Yohan Moren <yohan.moren@gmail.com>
2023-01-01 20:52:11 +0100

    commit for tmrw

--- ClickerReact/src/views/game.js
+++ ClickerReact/src/views/game.js
@@ -13,16 +13,22 @@ import {
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from 'axios';
 import GamePrestiges from '../files/prestige.json';
+import items from '../files/items.json';
 
 const Game = ({navigation}) => {
   let id = '63ac211bd72e2daa86cfe4c8';
   const [hasPassedPrestige, setHasPassedPrestige] = React.useState(false);
-  const prestigeTest = 10;
+  const [hasPassedItem, setHasPassedItem] = React.useState(false);
+  const [itemLevel, setItemLevel] = React.useState(0);
   const [score, setScore] = React.useState(0);
   const [prestige, setPrestige] = React.useState(0);
+
   useEffect(() => {
     if (score >= GamePrestiges.levels[prestige].cost) {
       setHasPassedPrestige(true);
+    }
+    if (score >= items.items[itemLevel].price) {
+      setHasPassedItem(true);
     }
   }, [score]);
 
@@ -52,7 +58,7 @@ const Game = ({navigation}) => {
   function addPrestige() {
     setScore(0);
     setPrestige(prestige + 1);
-   
+
     setHasPassedPrestige(false);
     AsyncStorage.setItem('prestige', prestige.toString());
     console.log(prestige);
@@ -70,13 +76,15 @@ const Game = ({navigation}) => {
         console.log(err);
       });
   }
+  // a faire demain
+  function addItem() {
+    setItem(item + 1);
+  }
 
   return (
     <SafeAreaView onStartShouldSetResponder={addScore}>
       <ClickView>
-        <BlueText>
-          {score}/{GamePrestiges.levels[prestige].cost}
-        </BlueText>
+        <BlueText>{score}</BlueText>
         <SettingsButton onPress={() => navigation.navigate('Settings')}>
           <TrophyIcon>Settings</TrophyIcon>
         </SettingsButton>
@@ -85,7 +93,25 @@ const Game = ({navigation}) => {
         </LeaderboardButton>
         {hasPassedPrestige ? <Button title="test" onPress={addPrestige} /> : ''}
       </ClickView>
+      <ItemCost>{items.items[itemLevel].price}</ItemCost>
+      <PrestigeCost>{GamePrestiges.levels[prestige].cost}</PrestigeCost>
     </SafeAreaView>
   );
 };
+
+const PrestigeCost = styled.Text`
+  position: absolute;
+  font-size: 20px;
+  right: 0;
+  bottom: 2%;
+  padding-right: 50px;
+`;
+
+const ItemCost = styled.Text`
+  position: absolute;
+  font-size: 20px;
+  bottom: 2%;
+  align-self: center;
+`;
+
 export default Game;
